a69110494e78b68fd19ebed78ae2ebdf8650959d
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-05 12:35:42 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,9 @@
-var plaulist = {
+var playlist = {
+  MichealJackson: "Man in the Mirror",
+  FrankSinatra: "Fly me to the Moon",
+  RayCharles: "Hit the Road Jack",
+}
+
+function updatePlaylist(object, string, value) {
   
 }
